228b5110a7e83f47bd511850cd43f319f23e2802
holydopeengine <holydopeengine@gmail.com>
2022-10-13 21:43:23 -0700

    printf task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Print a string
+* Description: use printf
+* Return: 0
+*/
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n";
+	return (0);
+}	
