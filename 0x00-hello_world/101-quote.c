9f2d60bab3857dddcfcf0bdb9b56a67558f09310
holydopeengine <holydopeengine@gmail.com>
2022-10-29 04:41:54 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,26 +1,21 @@
-@@ -0,0 +1,25 @@
-#include <stdio.h>
-
-/**
- * main - prints to string
- * Description: Prints "and that piece of art is useful.." without puts
- * Return: 1
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
  */
 
+#include <unistd.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar,2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
 int main(void)
 {
-	char *s= "and that piece of rt is useful\" - Dora korpar, 2015-10-19\n";
-	long l = 59;
-	long fd = 1;
-	long syscall = 1;
-	long ret =0;
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
-	__asm__("syscall"
-
-	: "=a" (ret)
-	: "a" (syscall),
-	"D" (fd),
-	"S" (s),
-	"d" (l));
 	return (1);
 }
